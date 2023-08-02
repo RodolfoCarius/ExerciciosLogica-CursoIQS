a3e77cd1c38bc000c5e8276efba9f0974ba546a3
RodolfoCarius <rcarius@gmail.com>
2023-08-02 20:43:13 -0300

    exercicio 1

---
+++ exercicio2/exercicio2.cs
@@ -0,0 +1,12 @@
+//Curso IQS - Lógica de programação
+
+// Exercicio 2
+// Escreva um algoritmo que leia três valores inteiros e diferentes e mostre-os em ordem
+// decrescente
+
+internal class Program
+{
+    public static void Main(string[] args)
+{
+}
+}
