398e5ec9d92b8f0ed517c176b4c7272d2aeab346
RodolfoCarius <rcarius@gmail.com>
2023-08-03 11:51:36 -0300

    exercicio 3

--- exercicio3/exercicio3.cs
+++ exercicio3/exercicio3.cs
@@ -8,3 +8,25 @@
 // ● É deficiente físico : Usa fila preferencial
 // ● É mulher gestante : Usa fila preferencial
 // O programa recebe como entrada a Idade, Sexo e a condição especial do usuário,se houver.
+
+int idade;
+string sexo;
+string condicao;
+
+Console.Write("Digite sua idade: ");
+idade = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("");
+
+Console.Write("Digite sue sexo: ");
+sexo = Console.ReadLine();
+Console.WriteLine("");
+
+Console.Write("Informe sua condição: ");
+condicao = Console.ReadLine();
+Console.WriteLine("");
+
+if(idade > 65 || condicao == "gestante" && sexo == "feminino" || condicao == "deficiente"){
+    Console.Write("Use a fila preferencial.");
+} else {
+    Console.Write("Use a fila comum.");
+}
