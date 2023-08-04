f0d8942a5535686123ac08d0147fa85238046f86
RodolfoCarius <rcarius@gmail.com>
2023-08-04 09:40:39 -0300

    exercicio 5

--- exercicio5/exercicio5.cs
+++ exercicio5/exercicio5.cs
@@ -4,3 +4,18 @@
 // Palavras palíndromos são aquelas que são lidas da direita para a esquerda da mesma
 // maneira que da esquerda para a direita. Exemplo ARARA. Dado uma palavra, informar se ela
 // é palíndroma ou não
+
+
+Console.WriteLine("Insira uma palavra:");
+string palavra = Console.ReadLine();
+
+string palavraReversa = new string(palavra.Reverse().ToArray());
+
+if(palavra == palavraReversa){
+    Console.WriteLine("Você inseriu uma palavra palíndromo!");
+}else{
+    Console.WriteLine("Está palavra não é palíndromo!");
+}
+    Console.WriteLine(palavra);
+    Console.WriteLine(palavraReversa);
+    
---
+++ exercicio6/exercicio6.cs
@@ -0,0 +1,5 @@
+﻿//Curso IQS - Lógica de programação
+
+// Exercicio 5
+// Escrever um algoritmo que leia um valor para uma variável N de 1 a 10 e calcule a tabuada
+// de N. Mostre a tabuada:
